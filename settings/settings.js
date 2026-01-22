efdb1339710ee7e82989a8ebe54f84fd1aaf762b
James Whaley <whaleyjames2001@gmail.com>
2026-01-22 09:21:05 -0600

    Added settings files

---
+++ settings/settings.js
@@ -0,0 +1,18 @@
+/*
+==========
+ Settings
+==========
+This class consists of static functions
+*/
+class Settings
+{
+    static readSettings(moduleName)
+    {
+        //TODO: Write this function. Use the browser's local storage.
+    }
+
+    static writeSettings(settings)
+    {
+        //TODO: Write this function. Use the browser's local storage.
+    }
+}
