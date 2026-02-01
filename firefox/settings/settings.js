bc45b96da782685e50f4837ec60842aede9ca1fc
James Whaley <whaleyjames2001@gmail.com>
2026-02-01 13:29:36 -0600

    Updated settings page
    Settings can now be set to defaults on installation. Page is successfully generated based on settings object. Object generation based on page has yet to be implemented.

--- firefox/settings/settings.js
+++ firefox/settings/settings.js
@@ -1,3 +1,7 @@
+// This file contains an object which contains various functions used for
+// creating a dynamic settings page.
+// Code by James Whaley
+
 /*
 ==========
  Settings
@@ -6,13 +10,148 @@ This class consists of static functions
 */
 class Settings
 {
-    static readSettings(moduleName)
+    static readStoredSettings(moduleName)
     {
-        //TODO: Write this function. Use the browser's local storage.
+        browser.storage.local.get(moduleName).then(
+            Settings.populateSettingsPage,
+            (err) =>
+            {
+                console.error(err);
+            }
+        );
     }
 
-    static writeSettings(settings)
+    static loadSettingsPage()
     {
-        //TODO: Write this function. Use the browser's local storage.
+        let fact = new ModuleFactory();
+
+        for (let i = 0; i < fact.getMaxModules(); i++)
+        {
+            Settings.readStoredSettings(
+                fact.getModuleName(i)
+            );
+        }
+    }
+
+    static populateSettingsPage(settings)
+    {
+        for (let setting in settings)
+        {
+            settings = settings[setting];
+        }
+
+        document.getElementById("settings_form").innerHTML +=
+        `<h2>${settings.moduleName}</h2>
+        `;
+
+        let fact = new ModuleFactory();
+        let options = fact.getModuleOptions(
+            fact.getModuleNo(settings.moduleName)
+        );
+
+        for (let option in options)
+        {
+            if (option === "moduleName")
+            { continue; }
+
+            let header = "<p>" + option + "</p>";
+
+            let type = options[option].type;
+
+            let id = settings.moduleName + "_" + option + "_" + type;
+
+            switch (type)
+            {
+                case "text":
+
+                    document.getElementById("settings_form").innerHTML +=
+
+                    `<label for="${id}">${option}: </label>
+                     <input type="text"
+                            id="${id}"
+                            name="${option + "_" + settings.moduleName}"
+                            value="${settings[option] ? settings[option] : ""}">
+                            <br>
+                    `;
+
+                    break;
+
+                case "text_list":
+
+                    document.getElementById("settings_form").innerHTML +=
+
+                    `<label for="${id}">${option}: </label><br>
+                     <textarea id="${id}"></textarea>
+                    `;
+
+                    settings[option].forEach((value) => {
+                        document.getElementById(`${id}`).innerHTML += value;
+                    })
+
+                    break;
+
+                case "select_exclusive":
+
+                    document.getElementById("settings_form").innerHTML +=
+                    `<p>${header}</p>
+                    `;
+
+                    for (let opt in options[option])
+                    {
+                        if (opt === "type" ||
+                            opt === "default" ||
+                            opt === "moduleName")
+                        { continue; }
+
+                        document.getElementById("settings_form").innerHTML +=
+
+                        `<input type="radio"
+                                id="${id + opt}"
+                                name="${option + "_" + settings.moduleName}"
+                                value=${opt}
+                                ${options[option][opt] === settings[option] ?
+                                    "checked" : ""}>
+                            <label for="${id}">${opt}</label><br>
+                        `;
+                    }
+
+                    break;
+
+                case "select_inclusive":
+
+                    document.getElementById("settings_form").innerHTML +=
+                    `<p>${header}</p>
+                    `;
+
+                    for (let opt in options[option])
+                    {
+                        if (opt === "type" ||
+                            opt === "default" ||
+                            opt === "moduleName")
+                        { continue; }
+
+                        document.getElementById("settings_form").innerHTML +=
+
+                        `<input type="checkbox"
+                                id="${id + opt}"
+                                name="${option + "_" + settings.moduleName}"
+                                value=${opt}
+                                ${options[option][opt] === settings[option] ?
+                                    "checked" : ""}>
+                        <label for="${id}">${opt}</label><br>
+                        `;
+                    }
+
+                    break;
+
+                default:
+                    throw new Error(`Settings: Undefined type for module ${settings.moduleName}:
+                                     ${type}`);
+
+                    break;
+            }
+        }
     }
 }
+
+document.addEventListener("DOMContentLoaded", Settings.loadSettingsPage)
