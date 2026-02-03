7dcf65a7ef1e1f40d86574f949c0bf2226e8998b
James Whaley <whaleyjames2001@gmail.com>
2026-02-03 11:28:48 -0600

    Update main.js

--- firefox/scripts/main.js
+++ firefox/scripts/main.js
@@ -1,20 +1,4 @@
 // TODO: Iterate through the numbers in the ModuleFactory class. Instantate
 //       each object, run its function, check the result, and destroy it.
 
-test = new TestModule();
 
-let settings =
-{
-    moduleName : "testModule",
-    enabled : true,
-    whitelistEnabled : true,
-    whitelist : true,
-    urlList : ["www.google.com", "www.duckduckgo.com"],
-
-    testThingOne : true,
-    testThingTwo : 2
-}
-
-test.setSettings(settings);
-
-test.scanPage();
