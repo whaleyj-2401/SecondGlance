c45051da5582f90f2cf864bd57daeff888dcb18f
James Whaley <whaleyjames2001@gmail.com>
2026-04-23 18:52:34 -0500

    Added license notice to source files.

--- extension/settings/settings.js
+++ extension/settings/settings.js
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2026 Andrew Detering, Kelowna Ngoyi, James Whaley
+ *
+ * This file is part of SecondGlance.
+ *
+ * SecondGlance is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * SecondGlance is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with SecondGlance. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // settings.js
 // This file contains an object which contains various functions used for
 // creating a dynamic settings page.
-// Code by James Whaley
+// Code by James Whaley and Kelowna Ngoyi
 
 let optionsArr = {
 
@@ -51,9 +70,9 @@ let optionsArr = {
     "Text Scanner" : {
         moduleName : "Text Scanner",
     
-        scanChunkSize : {
+        "Shortest Scannable Paragraph (Characters)" : {
             "type" : "text",
-            "default" : "500"
+            "default" : "100"
         },
             
         model : {
