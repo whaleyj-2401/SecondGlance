28d31b526b40f7a7e99bacef8a2d88b05a69a6e0
James Whaley <whaleyjames2001@gmail.com>
2026-04-26 16:46:25 -0500

    Update initChrome.js

--- extension/scripts/initChrome.js
+++ extension/scripts/initChrome.js
@@ -1,22 +1,3 @@
-/*
- * Copyright 2026 Andrew Detering, Kelowna Ngoyi, James Whaley
- *
- * This file is part of SecondGlance.
- *
- * SecondGlance is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * SecondGlance is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with SecondGlance. If not, see <https://www.gnu.org/licenses/>.
- */
-
 // initChrome.js
 // File contains code from the init.js file. It also contains code from
 // browser-polyfill.js. Code was consolidated here because service workers
@@ -1247,6 +1228,25 @@
              }
         });
 //# sourceMappingURL=browser-polyfill.js.map
+
+/*
+ * Copyright 2026 Andrew Detering, Kelowna Ngoyi, James Whaley
+ *
+ * This file is part of SecondGlance.
+ *
+ * SecondGlance is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * SecondGlance is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with SecondGlance. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // The following is code to be run on installation/update of the software.
 // The purpose of this code is to check for settings stored within the
